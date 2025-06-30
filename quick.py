6c92953829e4ed9dc4e83f6e3454339290a73433
Jinjian <leifmax@berkeley.edu>
2025-06-30 02:24:22 +0000

    Breaking change: UI support addition

--- quick.py
+++
@@ -1,5 +0,0 @@
-import faiss
-
-index_path = "/home/ubuntu/Jinjian/retrieval-scaling/built_index/index/index_IVFPQ.100000000.768.65536.64.faiss"
-index = faiss.read_index(index_path)
-print("Total vectors in index:", index.ntotal)
